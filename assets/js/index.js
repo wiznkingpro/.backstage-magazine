fde6cd44d5f32effc895d58d2faae5cf084422ae
WiznKing <142936186+wiznkingpro@users.noreply.github.com>
2025-10-21 15:30:38 +0700

    Add files via upload

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,23 +1,22 @@
 function openSearchMenu() {
 	const searchMenu = document.getElementById('search_menu');
 	searchMenu.classList.add('active');
-	document.body.style.overflow = 'hidden'; // запрет прокрутки
+	document.body.style.overflow = 'hidden'; 
 }
 
 function closeSearchMenu() {
 	const searchMenu = document.getElementById('search_menu');
 	searchMenu.classList.remove('active');
-	document.body.style.overflow = ''; // вернуть прокрутку
+	document.body.style.overflow = ''; //
 }
 
-// Закрытие по нажатию Esc
+
 document.addEventListener('keydown', (e) => {
 	if (e.key === 'Escape') {
 		closeSearchMenu();
 	}
 });
 
-// Закрытие по клику вне формы (опционально)
 document.getElementById('search_menu').addEventListener('click', (e) => {
 	if (e.target === document.getElementById('search_menu')) {
 		closeSearchMenu();
@@ -36,25 +35,39 @@ function closeMainMenu() {
 	document.body.style.overflow = '';
 }
 
-// Закрытие по Esc
 document.addEventListener('keydown', (e) => {
 	if (e.key === 'Escape') {
 		closeMainMenu();
-		closeSearchMenu(); // если оба могут быть открыты
+		closeSearchMenu();
 	}
 });
 
-// Закрытие по клику вне меню
 document.getElementById('main_menu').addEventListener('click', (e) => {
 	if (e.target === document.getElementById('main_menu')) {
 		closeMainMenu();
 	}
 });
 
-// Клик по пункту меню — закрыть
 document.querySelectorAll('.menu-item').forEach(item => {
 	item.addEventListener('click', () => {
-		// Можно добавить переход: window.location = '/news' и т.д.
 		closeMainMenu();
 	});
 });
+
+const scrollToTopBtn = document.getElementById('scrollToTop');
+
+window.addEventListener('scroll', () => {
+	if (window.scrollY > 300) {
+		scrollToTopBtn.classList.add('visible');
+	} else {
+		scrollToTopBtn.classList.remove('visible');
+	}
+});
+
+scrollToTopBtn.addEventListener('click', () => {
+	window.scrollTo({
+		top: 0,
+		behavior: 'smooth'
+	});
+});
+
